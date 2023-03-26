a510603e849fcdd16ba34953ba6ecbca52bf3dc9
Mirco Martin <mirco.martin@ecloudsolutions.com>
2023-03-26 11:32:33 -0300

    validaciones

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -6,16 +6,20 @@ export const useSearch = () => {
 	const [error, setError] = useState<string | null >(null)
 
 	useEffect(() => {
-		
-		if( query.trim().length < 2 ) {
-			setError('La busqueda debe tener al menos 3 caracteres');
-			return;
-		}
-		if( query.match(/^\d+$/) ) {
-			setError('No se puede buscar una pelicula con un numero')
-			return;
-		}
 
+		if ( query.trim().length <= 2 ) {
+
+				if( query.trim().length <= 2 ) {
+					setError('La busqueda debe tener al menos 3 caracteres');
+					return;
+				}
+				if( query.match(/^\d+$/) ) {
+					setError('No se puede buscar una pelicula con un numero')
+					return;
+				}
+
+		}
+		
 		setError( null )
 	}, [query])
 	
